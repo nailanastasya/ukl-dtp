11ddcf5da8fd884ad885229917153a4aa8ff7cc8
nailanastasya <nailah4nastasya.28@gmail.com>
2024-05-30 09:39:39 +0700

    update

--- src/screens/Category.js
+++ src/screens/Category.js
@@ -12,19 +12,24 @@ import Categories from "./Categories";
 const Category = (props) => {
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.searchBar}
-        placeholder="Search..."
-     
-      />
-    
+
+      <View ></View>
+      
+      <View style={styles.row}>
+      <Image source={require('../../assets/berita.png')} style={styles.img}/>
+      <Text style={styles.title}>SeeMeNow</Text>
+      </View>
+      <Text style={styles.subTitle}>News</Text>
      <Image source={require('../../assets/Mpu.png')} style={styles.displayImg} ></Image>
-     <TouchableOpacity style={styles.btnT} onPress={() => props.navigation.navigate(Place)}>
-        <Text style={styles.textBtn}>Place</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style={styles.btnT} onPress={() => props.navigation.navigate(Categories)}>
-        <Text style={styles.textBtn}>Category</Text>
-     </TouchableOpacity>
+     <View style={styles.listItem}>
+      <Text style={styles.textList}>
+        <Text>Papua - Tradisi Buka Sasi Laut digelar,</Text>
+        
+      </Text>
+      <Image source={require('../../assets/raja.png')} style={styles.imgList}></Image>
+      
+     </View>
+     
      
 
     </View>
@@ -40,9 +45,41 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
     backgroundColor: "#fff",
   },
+  listItem:{
+    marginTop:40,
+    flexDirection: 'row',
+    alignItems: 'center',
+  
+  },
+  textList:{
+    fontSize:10,
+  },
+  row:{
+    flexDirection: 'row',
+    alignItems: 'center',
+   
+  },
+  imgList:{
+    marginRight:200,
+    width:180,
+    height:100,
+    borderRadius:10,
+  },
+  subTitle:{
+    justifyContent:"center",
+    alignItems:"center",
+    fontSize:25,
+    marginLeft:140,
+  },
+  img:{
+    marginLeft:13,
+    marginTop:40,
+    width:40,
+    height:40,
+  },
   btnT:{
     marginTop:15,
-    backgroundColor: '#E5E5E5',
+    backgroundColor: '#fff',
     padding: 10,
     borderRadius: 10,
     
@@ -50,31 +87,33 @@ const styles = StyleSheet.create({
   textBtn:{
     color: '#121212',
     fontSize: 16,
-    fontWeight: 'bold',
+  
   },
   title: {
-    fontSize: 25,
+    marginTop:15,
+    fontSize: 20,
+    position:"relative",
     alignItems: "center",
     justifyContent: "center",
     
     marginTop: 30,
   },
-  subtitle: {
-    fontSize: 20,
-    color: "#474747",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 10,
+  // subtitle: {
+  //   fontSize: 20,
+  //   color: "#474747",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  //   marginTop: 10,
     
-  },
-  text: {
+  // },
+  // text: {
     
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: 18,
-    paddingRight: 80,
-    lineHeight: 25,
-  },
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  //   fontSize: 18,
+  //   paddingRight: 80,
+  //   lineHeight: 25,
+  // },
 //   btn: {
 //     backgroundColor: "#E2443B",
 //     // alignItems: "center",
@@ -89,76 +128,76 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#FFF",
   },
-  cont1: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "100%",
-    justifyContent: "space-between",
-    marginTop: 40,
-  },
-  c3: {
-    height: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    width: 20,
-    borderRadius: 15,
-    backgroundColor: "#529CC0",
-  },
-  c2: {
-    height: 20,
-    width: 20,
-    borderRadius: 15,
-    backgroundColor: "#529C47",
-    marginHorizontal:10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  c1: {
-    height: 20,
-    width: 20,
-    borderRadius: 15,
-    backgroundColor: "#E2443B",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  selected: {
-    borderColor: "#E2443B",
-    height: 30,
-    width: 30,
-    borderRadius: 24,
-    borderWidth: 2,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  cont2: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "100%",
-    marginVertical: 25,
-  },
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100%",
-    paddingHorizontal: 20,
-    paddingTop: 50,
-  },
-  img: {
-    height: "45%",
-    width: "50%",
-  },
-  cont3: {
-    flex: 1,
-    backgroundColor: "#FFF",
-    width: "100%",
-    borderRadius: 50,
-    paddingHorizontal: 20,
-  },
+  // cont1: {
+  //   flexDirection: "row",
+  //   alignItems: "center",
+  //   width: "100%",
+  //   justifyContent: "space-between",
+  //   marginTop: 40,
+  // },
+  // c3: {
+  //   height: 20,
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  //   width: 20,
+  //   borderRadius: 15,
+  //   backgroundColor: "#529CC0",
+  // },
+  // c2: {
+  //   height: 20,
+  //   width: 20,
+  //   borderRadius: 15,
+  //   backgroundColor: "#529C47",
+  //   marginHorizontal:10,
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  // c1: {
+  //   height: 20,
+  //   width: 20,
+  //   borderRadius: 15,
+  //   backgroundColor: "#E2443B",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  // selected: {
+  //   borderColor: "#E2443B",
+  //   height: 30,
+  //   width: 30,
+  //   borderRadius: 24,
+  //   borderWidth: 2,
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  // cont2: {
+  //   flexDirection: "row",
+  //   alignItems: "center",
+  //   width: "100%",
+  //   marginVertical: 25,
+  // },
+  // header: {
+  //   flexDirection: "row",
+  //   alignItems: "center",
+  //   justifyContent: "space-between",
+  //   width: "100%",
+  //   paddingHorizontal: 20,
+  //   paddingTop: 50,
+  // },
+  // img: {
+  //   height: "45%",
+  //   width: "50%",
+  // },
+  // cont3: {
+  //   flex: 1,
+  //   backgroundColor: "#FFF",
+  //   width: "100%",
+  //   borderRadius: 50,
+  //   paddingHorizontal: 20,
+  // },
   buttonImage: {
     height:30,
     width:30,
-    marginTop:700,
+    marginTop:1000,
     marginRight:150,
   },
   button1: {
@@ -170,11 +209,11 @@ const styles = StyleSheet.create({
   displayImg:{
     // position:'absolute',
     height:175,
+    marginTop:20,
     marginLeft:5,
-    marginTop: 100,
     width:350,
     paddingLeft:100,
-    borderRadius:25,
+    borderRadius:15,
   },
   btnImage1:{
     backgroundColor:''
--- src/screens/Place.js
+++ src/screens/Place.js
@@ -1,9 +1,13 @@
-import { View, Text, Image, FastImage, StyleSheet } from 'react-native'
+import { View, Text, Image, FastImage, StyleSheet, ScrollView,TextInput, FlatList, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 
-export default function Place() {
+const Place = () => {
+  
   const [places, setPlaces] = useState([]);
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [query, setQuery] = useState('');
   
   const getPlaces = async () => {
     const response = await fetch("https://dewalaravel.com/api/places");
@@ -17,29 +21,83 @@ export default function Place() {
   useEffect(() => {
     getPlaces();
   }, []);
+
+  const handleSearch = (text) => {
+    setQuery(text);
+    if (text) {
+      const filtered = data.filter(place => place.name.toLowerCase().includes(text.toLowerCase()));
+      setFilteredData(filtered);
+    } else {
+      setFilteredData(data);
+    }
+  };
+  
+  const handleCancel = () => {
+    setQuery('');
+    setFilteredData(data);
+  };
 return(
+  <ScrollView>
+     <FlatList
+        data={filteredData}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(place) => place.id.toString()}
+        renderItem={({ place }) => (
+          <TouchableOpacity style={styles.placeContainer} onPress={() => router.push(`/place/${place.slug}`)}>
+            <Image source={{ uri: place.photo }} style={styles.placeImage} />
+            <View style={styles.itemTextContainer}>
+              <Text style={styles.itemName}>{place.name}</Text>
+              <Text style={styles.itemCategory}>{item.category.name}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+   < TextInput
+        style={styles.searchBar}
+        placeholder="Search..." value={query}
+        onChangeText={handleSearch}
+     
+      />
+       <TouchableOpacity onPress={handleCancel} style={styles.cancelButton}></TouchableOpacity>
   <View style={{
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   }}>
+  
     {places.data ? (
       places.data.map((place, index) => (
         <View key={index}>
-          
-          <Text>{place.name}</Text>
+        <TouchableOpacity>
+          <Text style={styles.placeName}>{place.name}</Text>
+          <Image source={{ uri: place.photo}} style={styles.image} />
+          {/* <Text>{place.description}</Text> */}
+          </TouchableOpacity>
         </View>
       ))
     ) : (
       <Text>Loading</Text>
     )}
   </View>
+  </ScrollView>
 );
 };
 const styles = StyleSheet.create({
   image: {
-    width:150,
+    marginLeft:5,
+    width:300,
     height:150,
+    borderRadius:10,
   },
+ searchBar:{
+  margin:40,
+ },
+ placeName:{
+  marginLeft:10,
+
+ },
+
 
 });
+
+export default Place;
